6b30196f54b140560c41a4865ae59e856f7329da
ichiro18 <ichirozcom@gmail.com>
2018-03-21 18:04:01 +0300

    add post grid

---
+++ site/web/app/themes/sage/category.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Template Name: Навигационная страница
+ */
+?>
+
+<?php
+$category_id = get_the_ID();
+$posts_query = new WP_Query(array(
+	'post_type'=>'post',
+	'post_status'=>'publish',
+	'posts_per_page'=>10
+));
+?>
+<div class="wrapper">
+  <pre><?php var_dump($posts_query); ?></pre>
+  <h3 class="page-title"><?php single_cat_title(); ?></h3>
+  <?php
+    $term_description = term_description();
+    if (!empty($term_description)) {
+  ?>
+    <div class="page-description">
+        <?php echo $term_description; ?>
+    </div>
+  <?php }; ?>
+  <div class="page-wrapper">
+    <div class="main-content <?php if (is_active_sidebar( 'sidebar-primary' )){ echo "active-sidebar";}; ?>">
+        <?php if ($posts_query->have_posts()) { ?>
+	        <?php
+                $posts = $posts_query->posts;
+//                var_dump($posts);
+	        ?>
+            <div class="posts-easy-grid">
+                <?php foreach ($posts as $post) { ?>
+                    <article class="post-item" role="article">
+                        <a
+                                class="post-thumbnail"
+                                style='background-image: url("<?php echo get_the_post_thumbnail_url($post->ID); ?>");'
+                                title="<?php echo get_the_post_thumbnail_caption($post->ID); ?>"
+                                href="<?php echo get_permalink($post->ID, false); ?>"
+                        >
+                        </a>
+                        <header class="post-header">
+                            <h1>
+                                <a href="<?php echo get_permalink($post->ID, false); ?>"><?php echo $post->post_title; ?></a>
+                            </h1>
+                        </header>
+                        <footer class="post-footer">
+                            <ul class="tag-list">
+                                <?php echo get_the_tag_list("<li>","</li><li>","</li>"); ?>
+                            </ul>
+                            <div class="date">
+	                            <?php
+                                    the_modified_date("j F Y - H:i");
+                                ?>
+                            </div>
+                        </footer>
+                    </article>
+                <?php } ?>
+            </div>
+        <?php wp_reset_postdata(); ?>
+        <?php
+        } else { ?>
+            <div class="alert alert-danger full-width" role="alert">
+                В данной рубрике отстутствуют записи
+            </div>
+        <?php }; ?>
+    </div>
+    <?php
+      get_template_part("templates/sidebar");
+    ?>
+  </div>
+</div>
+
